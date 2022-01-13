d8cb642a685abbdb8209e5fb2c4fe77f2c5b219e
Caleb Morales <CMorales2@sorenson.com>
2022-01-13 15:19:00 -0700

    gets weather data, added loading icon, correctly displays temperature

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,56 @@
+import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+const String API_KEY = 'your api key here';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    Location location = new Location();
+
+    await location.getCurrentLocation();
+
+    Uri uri = Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$API_KEY&units=imperial');
+
+    NetworkHelper networkHelper = NetworkHelper(url: uri);
+
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen(
+        locationWeather: weatherData,
+      );
+    }));
+  }
+
+  void getData() async {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
